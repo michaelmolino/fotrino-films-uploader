f4e8d147d5ebb85daac879b089af3894c0cb62e2
Michael Molino <creative@michaelmolino.com>
2025-02-19 22:30:58 +0000

    upload image support from browser

--- fotrino-upload.sh
+++ fotrino-upload.sh
@@ -18,7 +18,7 @@ while getopts ":d" OPTION; do
             minio_web_root='http://localhost:9000/fotrino'
             ;;
         ?)
-            echo "Usage: $(basename "$0") [-d] /path/to/Media"
+            echo "Usage: $(basename "$0") [-d] /path/to/Media.mov"
             exit 1
             ;;
     esac
@@ -29,10 +29,10 @@ shopt -s nullglob
 
 # Check OS
 os=$(uname)
-[[ "$os" = 'Darwin' ]] || { echo "Untested Operating System!"; read -r -p "Press enter to continue."; }
+[[ "$os" = 'Darwin' ]] || { echo "I've only tested this on Mac."; read -r -p "Press enter to continue anyway."; }
 
 # Check dependencies
-declare -a deps=("${video2hls}/video2hls" 'md5sum' 'tar' 'ffmpeg' 'gm' 'jq' 'curl')
+declare -a deps=("${video2hls}/video2hls" 'md5sum' 'tar' 'ffmpeg' 'jq' 'curl')
 for dep in "${deps[@]}"; do
     which "$dep" &>/dev/null || { echo "Missing ${dep}..."; fail=1; }
 done
@@ -48,7 +48,7 @@ git --git-dir="${video2hls}/.git/" --work-tree="${video2hls}/" fetch
 [[ $(git --git-dir="${video2hls}/.git/" --work-tree="${video2hls}/" status 2>/dev/null |grep -c "Your branch is up to date with" ) == 1 ]] || { echo "video2hls must be updated..."; fail=1; }
 
 # Check command line parameter(s)
-[[ -n $1 && -d "$1" ]] || { echo "Usage: $(basename "$0") [-d] /path/to/Media"; fail=1; }
+[[ -n $1 && -f "$1" ]] || { echo "Usage: $(basename "$0") [-d] /path/to/Media.mov"; fail=1; }
 
 # STOP if there are errors
 [[ $fail -eq 1 ]] && exit 1
@@ -58,19 +58,7 @@ read -r -p "Enter tokens JSON: " tokens
 uploadToken=$(echo "$tokens" | jq -r '.uploadToken')
 userToken=$(echo "$tokens" | jq -r '.userToken')
 status=$(curl -s -o /dev/null -w "%{http_code}" $insecure -s -H "Authorization: Bearer $userToken" ${api}/api/upload/hello)
-[[ "$status" == "200" ]] || { echo "Unable to validate tokens..."; exit 1; }
-
-# Get metadata
-metadata=$(curl -s -w "%{http_code}" $insecure -H "Authorization: Bearer $userToken" -H "X-Upload-Token: $uploadToken" ${api}/api/upload/metadata)
-http_status="${metadata: -3}"
-if [[ "$http_status" == "200" ]]; then
-    metadata="${metadata%???}"
-    channel_pending=$(echo "$metadata" | jq -r '.channel_pending')
-    project_pending=$(echo "$metadata" | jq -r '.project_pending')
-else
-    echo "Unable to validate tokens..."
-    exit 1;
-fi
+[[ "$status" == "200" ]] || { echo "Unable to validate user token..."; exit 1; }
 
 # STOP if there are errors
 [[ $fail -eq 1 ]] && exit 1
@@ -79,38 +67,15 @@ fi
 files=("${1}/"Media.{mp4,mov,webm})
 [[ ${#files[@]} -gt 0 ]] || { echo "Missing Media..."; fail=1; }
 
-files=("${1}/"Preview.{jpg,jpeg,png,webp})
-[[ ${#files[@]} -gt 0 ]] || { echo "Missing Preview..."; fail=1; }
-
-if [[ "$project_pending" == true ]]; then
-    files=("${1}/"Poster.{jpg,jpeg,png,webp})
-    [[ ${#files[@]} -gt 0 ]] || { echo "Missing Poster..."; fail=1; }
-fi
-
-if [[ "$channel_pending" == true ]]; then
-    files=("${1}/"Cover.{jpg,jpeg,png,webp})
-    [[ ${#files[@]} -gt 0 ]] || { echo "Missing Cover..."; fail=1; }
-fi
-
 # STOP if there are errors
 [[ $fail -eq 1 ]] && exit 1
 
 # Process Media
 files=("${1}/"Media.{mp4,mov,webm})
 [[ ${#files[@]} -gt 0 ]] && { 
-    "${video2hls}/video2hls" --no-poster --no-mp4 "${1}/"Media.* || { echo "Media failed to convert..."; exit 1; } 
+    "${video2hls}/video2hls" --no-poster --no-mp4 "${1}" || { echo "Media failed to convert..."; exit 1; } 
     :
 }
-
-# Optimise Images
-for file in "${1}/"*.{jpg,jpeg,png,webp}; do
-    if [[ ! "$file" =~ _opt\.(jpg|jpeg|png|webp)$ ]]; then
-        dir="$(dirname "${file}")"
-        filename=$(basename -- "${file}")
-        basefile="${filename%.*}"
-        gm convert -resize 720x720 -strip -interlace Plane -quality 80 "${file}" "${dir}/${basefile}_opt.jpg" || { echo "$filename failed to process..."; exit 1; }
-    fi
-done
 
 echo
 read -r -p "Make sure there are no errors before continuing with upload. You can safely ignore the 'cannot extract codec' warning or any warnings about skipped resolutions. Press enter to continue."
@@ -134,39 +99,21 @@ process_object () {
 echo
 echo "$(date) Starting upload..."
 
-if [[ "$channel_pending" == true ]]; then
-    file="${1}/Cover_opt.jpg"
-    cover="covers/$(md5sum "${file}"|awk '{print $1}').jpg"
-    process_object "$file" "$cover" "image/jpeg"
-    coverUrl="${minio_web_root}/${cover}"
-fi
-
-if [[ "$project_pending" == true ]]; then
-    file="${1}/Poster_opt.jpg"
-    poster="posters/$(md5sum "${file}"|awk '{print $1}').jpg"
-    process_object "$file" "$poster" "image/jpeg"
-    posterUrl="${minio_web_root}/${poster}"
-fi
-
-file="${1}/Preview_opt.jpg"
-preview="previews/$(md5sum "${file}"|awk '{print $1}').jpg"
-process_object "$file" "$preview" "image/jpeg"
-previewUrl="${minio_web_root}/${preview}"
-
-hash=$(tar -C / -cf - "${1:1}/Media" |md5sum |awk '{print $1}')
-for file in "${1}"/Media/*.ts; do
+media_folder="${1%.*}"
+hash=$(tar -C / -cf - "${media_folder:1}" |md5sum |awk '{print $1}')
+for file in "${media_folder}"/*.ts; do
     object="media/${hash}/$(basename "$file")"
     process_object "$file" "$object" "video/mp4"
 done
 
-for file in "${1}"/Media/*.m3u8; do
+for file in "${media_folder}"/*.m3u8; do
     object="media/${hash}/$(basename "$file")"
     process_object "$file" "$object" "text/plain"
 done
 srcUrl="${minio_web_root}/media/${hash}/index.m3u8"
 
 # Change pending status to false
-payload="{ \"cover\": \"$coverUrl\", \"poster\": \"$posterUrl\", \"preview\": \"$previewUrl\", \"src\": \"$srcUrl\", \"type\": \"application/vnd.apple.mpegurl\" }"
+payload="{ \"src\": \"$srcUrl\", \"type\": \"application/vnd.apple.mpegurl\" }"
 while true; do
     status=$(curl -s -o /dev/null -w "%{http_code}" $insecure \
         -H "Authorization: Bearer $userToken" \
@@ -182,3 +129,16 @@ while true; do
 done
 
 echo "Success!"
+
+# NOTES
+# I should replace video2hls with ffmpeg
+# ffmpeg -i input.mp4 -map 0:v:0 -map 0:a:0 -c:a aac -b:a 128k -c:v h264 -crf 23 -g 48 -sc_threshold 0 \
+#   -filter:v:0 "scale=-2:240"  -b:v:0 400k  -maxrate:v:0 450k  -bufsize:v:0 800k  -preset faster \
+#   -filter:v:1 "scale=-2:480"  -b:v:1 800k  -maxrate:v:1 900k  -bufsize:v:1 1600k  -preset faster \
+#   -filter:v:2 "scale=-2:720"  -b:v:2 2800k -maxrate:v:2 3000k -bufsize:v:2 5600k -preset faster \
+#   -filter:v:3 "scale=-2:1080" -b:v:3 5000k -maxrate:v:3 5500k -bufsize:v:3 10000k -preset faster \
+#   -f hls -hls_time 6 -hls_playlist_type vod \
+#   -hls_segment_filename "hls/%v/segment_%03d.ts" \
+#   -master_pl_name "hls/master.m3u8" \
+#   -var_stream_map "v:0,a:0 v:1,a:0 v:2,a:0 v:3,a:0" \
+#   "hls/%v/index.m3u8"
